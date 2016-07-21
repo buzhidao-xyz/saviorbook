c8d56fbc0d2b5e334b1037a51245a27eba731bd2
buzhidao <imbzd.wang@aliyun.com>
2016-07-21 15:57:11 +0800

    Chapter页

--- Mobile/js/controller/book.controller.js
+++ Mobile/js/controller/book.controller.js
@@ -24,7 +24,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 				$("#ChapterBox").removeClass('pt-page-moveToTop').addClass('pt-page-moveFromTop').show();
 
 				//service交互 - getChapterList
-				// $BookService.getChapterList();
+				$BookService.getChapterList();
 				//监听事件 - getChapterList.success
 				$scope.$on('getChapterList.success', function (event, d){
 					$scope.$chapterlist = $BookService.chapterlist;
@@ -33,13 +33,64 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 
 			//aboutus-text
 			$scope.aboutusTextScroll = function () {
+				$.fn.myScroll = function(options){
+					var defaults = {
+						speed: 40,
+						rowHeight: 24
+					};
 				
+					var opts = $.extend({}, defaults, options),intId = [];
+				
+					function marquee(obj, step){
+						obj.find("ul").animate({
+							marginTop: '-=1'
+						},0,function(){
+							var s = Math.abs(parseInt($(this).css("margin-top")));
+							if(s >= step){
+								$(this).find("li").slice(0, 1).appendTo($(this));
+								$(this).css("margin-top", 0);
+							}
+						});
+					}
+					this.each(function(i){
+						var sh = opts["rowHeight"],speed = opts["speed"],_this = $(this);
+						intId[i] = setInterval(function(){
+							if(_this.find("ul").height()<=_this.height()){
+								clearInterval(intId[i]);
+							}else{
+								marquee(_this, sh);
+							}
+						}, speed);
+
+						_this.hover(function(){
+							clearInterval(intId[i]);
+						},function(){
+							intId[i] = setInterval(function(){
+								if(_this.find("ul").height()<=_this.height()){
+									clearInterval(intId[i]);
+								}else{
+									marquee(_this, sh);
+								}
+							}, speed);
+						});
+					});
+				}
+
+				setTimeout(function () {
+					var liHeight = parseInt($('.aboutus_text_box').find("ul li").css("height"));
+					$('.aboutus_text_box').myScroll({
+						speed: 50, //数值越大，速度越慢
+						rowHeight: liHeight //li的高度
+					});
+				}, 500);
 			}
 
 			//页面逻辑
 			var path = $route.current.originalPath;
 			switch (path) {
 				case '/book':
+					break;
+				case '/chapter/chapterid/:chapterid':
 					break;
 				case '/aboutus':
 					$scope.aboutusTextScroll();
