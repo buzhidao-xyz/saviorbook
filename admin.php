dbb0ca79560a556dbed7853565d18dc0a04978f2
buzhidao <imbzd.wang@aliyun.com>
2016-07-22 17:44:04 +0800

    Api

--- admin.php
+++ admin.php
@@ -10,9 +10,6 @@
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
-header('Mobile/');
-exit;
-
 //开启缓冲Buffer 用ob_gzhandler回调来判断是否压缩输出gzip
 ob_start('ob_gzhandler');
 
@@ -20,7 +17,7 @@ ob_start('ob_gzhandler');
 define('APP_DEBUG', True);
 
 //绑定主入口模块 例：绑定Front模块 可以直接访问Front模块的controller/action
-define('MAIN_MODULE', 'Mobile');
+define('MAIN_MODULE', 'Admin');
 
 //系统应用主入口标识
 define('APP_INDEX', 1);
