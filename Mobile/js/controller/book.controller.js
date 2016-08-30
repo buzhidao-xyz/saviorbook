20c93f887f4f639d3ce87a12f7517959b6e02687
imbzd <luochuan.wang@gmail.com>
2016-08-30 08:23:57 +0800

    speed85

--- Mobile/js/controller/book.controller.js
+++ Mobile/js/controller/book.controller.js
@@ -86,7 +86,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 				$.fn.myScroll = function(options){
 					var flag = 1;
 					var defaults = {
-						speed: 100,
+						speed: 85,
 						rowHeight: 471.41
 					};
 				
@@ -136,7 +136,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 					setTimeout(function (){
 						var liHeight = $('.aboutus_text_box').find("img.aboutus_text").height()+10;
 						$('.aboutus_text_box').myScroll({
-							speed: 100, //数值越大，速度越慢
+							speed: 85, //数值越大，速度越慢
 							rowHeight: liHeight //li的高度
 						});
 					}, 100);
