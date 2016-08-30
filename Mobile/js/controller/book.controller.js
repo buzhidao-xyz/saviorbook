cff8cdcca82026d0a304431861646b48a3e06a85
imbzd <luochuan.wang@gmail.com>
2016-08-30 08:31:49 +0800

    speed5

--- Mobile/js/controller/book.controller.js
+++ Mobile/js/controller/book.controller.js
@@ -86,7 +86,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 				$.fn.myScroll = function(options){
 					var flag = 1;
 					var defaults = {
-						speed: 30,
+						speed: 5,
 						rowHeight: 471.41
 					};
 				
@@ -96,7 +96,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 
 					function marquee(obj, step){
 						obj.find("ul").animate({
-							marginTop: '-=0.5'
+							marginTop: '-=0.2'
 						},5,function(){
 							var s = Math.abs(parseInt($(this).css("margin-top")));
 							if (s >= step) {
@@ -136,7 +136,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 					setTimeout(function (){
 						var liHeight = $('.aboutus_text_box').find("img.aboutus_text").height()+10;
 						$('.aboutus_text_box').myScroll({
-							speed: 30, //数值越大，速度越慢
+							speed: 5, //数值越大，速度越慢
 							rowHeight: liHeight //li的高度
 						});
 					}, 100);
