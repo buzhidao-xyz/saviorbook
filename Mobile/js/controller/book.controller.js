f046ffe43416ee65d86ab91eba9897dc779a1a1e
imbzd <luochuan.wang@gmail.com>
2016-08-30 08:21:13 +0800

    speed3000

--- Mobile/js/controller/book.controller.js
+++ Mobile/js/controller/book.controller.js
@@ -86,7 +86,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 				$.fn.myScroll = function(options){
 					var flag = 1;
 					var defaults = {
-						speed: 50,
+						speed: 3000,
 						rowHeight: 471.41
 					};
 				
@@ -97,7 +97,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 					function marquee(obj, step){
 						obj.find("ul").animate({
 							marginTop: '-=1'
-						},10,function(){
+						},0,function(){
 							var s = Math.abs(parseInt($(this).css("margin-top")));
 							if (s >= step) {
 								$(this).find("li").slice(0, 1).appendTo($(this));
@@ -136,7 +136,7 @@ define(["require", "app", "commoncontroller"], function ($require, $app){
 					setTimeout(function (){
 						var liHeight = $('.aboutus_text_box').find("img.aboutus_text").height()+10;
 						$('.aboutus_text_box').myScroll({
-							speed: 50, //数值越大，速度越慢
+							speed: 3000, //数值越大，速度越慢
 							rowHeight: liHeight //li的高度
 						});
 					}, 100);
